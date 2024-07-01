5e6ba648c10b402c9741e40514e9efb201753ebc
nu11secur1ty <bugb0untypropentest@gmail.com>
2024-07-01 15:33:16 +0300

    Update updateUB.sh

--- updateUB.sh
+++ updateUB.sh
@@ -2,6 +2,9 @@
 docker stop ub1nu11
 echo "Please wait..."
 sleep 5;
+cd ${HOME}
+rm -rf ub1nu11
+git clone https://github.com/f0rc3ps/ub1nu11.git
 cd ${HOME}/ub1nu11
 docker-compose -f docker-compose.yml down -v
 docker-compose up -d
