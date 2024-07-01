fad476c51bd339e4a9ba62d4734f378a11f957f1
nu11secur1ty <bugb0untypropentest@gmail.com>
2024-07-01 22:40:23 +0300

    Update updateUB.sh

--- updateUB.sh
+++ updateUB.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/bash
-cd ${HOME}/ub1nu11
+cd ${HOME}
 docker stop ub1nu11
 docker system prune -a
 echo "Please wait..."
 sleep 5;
-cd ${HOME}
 rm -rf ub1nu11
 git clone https://github.com/f0rc3ps/ub1nu11.git
 cd ${HOME}/ub1nu11
