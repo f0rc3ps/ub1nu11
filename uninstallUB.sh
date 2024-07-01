a5f6915ae3fc9b2c2d18dcfa36ec8f126cf8cb32
nu11secur1ty <bugb0untypropentest@gmail.com>
2024-07-01 14:54:28 +0300

    Update uninstallUB.sh

--- uninstallUB.sh
+++ uninstallUB.sh
@@ -7,8 +7,11 @@ apt autoremove -y
 
 # Clean docker
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sleep 3;
 systemctl stop docker.socket
+sleep 5;
 systemctl stop docker.service
+sleep 5;
 sudo rm -rf /var/lib/docker 
 sudo rm -rf /etc/docker
 cd /etc/apt/sources.list.d/
