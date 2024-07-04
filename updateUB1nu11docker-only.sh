a94a2a41b441700c8ffb099c741bd89b04391d7c
nu11secur1ty <bugb0untypropentest@gmail.com>
2024-07-04 20:53:40 +0300

    Update updateUB1nu11docker-only.sh

--- updateUB1nu11docker-only.sh
+++ updateUB1nu11docker-only.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/bash
 cd ${HOME}
+
+# clean
 docker stop ub1nu11
 docker system prune -a
 echo "Please wait..."
 sleep 5;
 rm -rf ub1nu11
+
+# get ub1nu11 
 git clone https://github.com/f0rc3ps/ub1nu11.git
-cd ub1nu11/docker
+
+# set ip
+cd ub1nu11/docker/
+chmod a+x setenvip.sh
+bash setenvip.sh
 docker-compose -f docker/docker-compose.yml up -d
+
 cd ${HOME}
 exit 0;
