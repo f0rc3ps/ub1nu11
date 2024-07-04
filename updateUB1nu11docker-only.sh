c02800ef4fd0e216f4971db1be00ba86322a9a56
nu11secur1ty <bugb0untypropentest@gmail.com>
2024-07-04 18:42:38 +0300

    Update updateUB1nu11docker-only.sh

--- updateUB1nu11docker-only.sh
+++ updateUB1nu11docker-only.sh
@@ -4,8 +4,8 @@ docker stop ub1nu11
 docker system prune -a
 echo "Please wait..."
 sleep 5;
-rm -rf ub1nu11
-git clone https://github.com/f0rc3ps/ub1nu11.git
+# rm -rf ub1nu11
+# git clone https://github.com/f0rc3ps/ub1nu11.git
 cd ub1nu11
 docker-compose -f docker/docker-compose.yml up -d
 cd ${HOME}
